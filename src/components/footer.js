da131449ff16e60b3753bf401104b299d0b376dc
Charles Uthulor <charlesnnanna1667@gmail.com>
2020-04-17 08:20:30 -0700

    Added the footer component

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FooterStyles from './footer.module.css'
+
+const Footer = () => {
+    return(
+        
+            <div >
+                <p className = {FooterStyles.footer1}>Copyright 2020 Charles Uthulor
+                </p>
+                <p className = {FooterStyles.footer2}>Charles Uthulor Web Design Portfolio</p>
+                </div>
+        
+    )
+}
+
+export default Footer;
